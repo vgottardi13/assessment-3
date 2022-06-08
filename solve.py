059946fa32417fd5f257b4ae1c6d1eec4bb8907d
vanessa_gottardi <vanessa.gottardi@live.vu.edu.au>
2022-06-08 18:22:59 +1000

    1. had a problem with pandas 2.solved the problem, now able to run code 3. pandas used to read the 3 files (pc.json, orders.csv, order_details.csv) from 3 different database systems. (sales records of a retail business in July 2021). 4. Answer question 1: estimate the budget of the freights in the next month for this business a. First we use the sum() method to add all values in each column. b. Then we calculate low, mid, high range of the freights by 5. question 2 answer 6. question 3 answer 7. question 4 answer

--- solve.py
+++ solve.py
@@ -1,46 +1,46 @@
 import pandas as pd
-
-
 orders_df = pd.read_csv("orders.csv")
 order_details_df = pd.read_csv("order_details.csv")
 product_categories_df = pd.read_json("pc.json")
-
 period = "July 2021"
 
-print(f"\nThis is an estimation of the budget of the freights in the next month for this business. See results below:")
+print("\n Current Freight Budget : ", orders_df['freight'].sum())
+print(f"\nThis is an estimation of the freight budget for the next month based on the above freight charges.")
 
 def question_1(low,mid,high):
     total_freight = orders_df['freight'].sum()
     result_str = f"\nThe total freight expense for {period} is ${total_freight:,.2f}. Based on this, below estimates are provided.\n\n"
-    result_str = f"low range budget increase {low}% is ${total_freight * (1 + low/100):,.2f}\n"
-    result_str = f"mid range budget increase {mid}% is ${total_freight * (1 + mid/100):,.2f}\n"
-    print("\n")
-    result_str = f"High range budget increase {high}% is ${total_freight * (1 + high/ 100):,.2f}\n"
+    result_str = f"Low range budget increase {low}% is ${total_freight * (1 + low/100):,.2f}\n"
+    result_str = f"Mid range budget increase {mid}% is ${total_freight * (1 + mid/100):,.2f}\n"
+    result_str = f"Estimated freight budget(high range {high}%)  is ${total_freight * (1 + high/ 100):,.2f}\n"
     return result_str
 
-print(question_1(5,10,15))
+print(question_1(5,10,25))
 
-def question_2(top_n_rows):
-    orders_group_by_country = orders_df.groupby('ship_country').size().nlargest(top_n_rows).reset_index()
-    result_str = f"\nTop {top_n_rows} destination countries with number of orders placed in {period}.\n\n"
-    result_str += "Country              No. of orders\n"
-    result_str += "---------            -------------\n"
-    for row in range(0,top_n_rows):
-        result_str += f"{orders_group_by_country.iloc[row, 0]:<25}{orders_group_by_country.iloc[row, 1]:>5}\n"
-        return result_str
 
-print(question_2(3))
+# Question 2:
+orders_by_country = orders_df.groupby('ship_country').size().sort_values(ascending=False)
+print("\nTop 2 destination countries with number of orders placed in ", period)
+print("Country    No. of orders")
+print("----------------------")
+print(orders_by_country.index[0],"    ", orders_by_country.iloc[0])
+print(orders_by_country.index[1],"        ",orders_by_country.iloc[1])
 
-def question_3(top_n_rows):
-    orders_group_by_customer = orders_df.groupby('customer_name').size().nlargest(top_n_rows).reset_index()
-    result_str = f"\nTop {top_n_rows} customers with number of orders placed in {period}.\n\n"
-    result_str += "Country              No. of orders\n"
-    result_str += "---------            -------------\n"
-    for row in range(0, top_n_rows):
-        result_str += f"{orders_group_by_customer.iloc[row, 0]:<25}{orders_group_by_customer.iloc[row, 1]:>5}\n"
-        return result_str
 
-print(question_3(3))
+
+
+# Question 3 :
+def top_customers(no):
+    orders_group_by_customer = orders_df.groupby('customer_name').size().nlargest(no).reset_index()
+    print("\nTop 3 customers with number of orders placed in", period)
+    print("\nCustomers        No. of orders")
+    print("---------        -------------")
+    for i in range(0, no):
+        print(orders_group_by_customer.iloc[i, 0],"      ",orders_group_by_customer.iloc[i, 1])
+
+top_customers(3)
+
+# Question 4 :
 
 def question_4(top_n):
     order_details_df["total_price"] = order_details_df["unit_price"] * order_details_df["quantity"]
@@ -51,7 +51,7 @@ def question_4(top_n):
     result_str += "--------------------     --------------------              ------------------           -----------------      \n"
     for row in range(top_n):
         result_str += f"{top_sale.iloc[row, 0]:>15}      ${top_sale.iloc[row, 4]:>10,.02f}"
-        result_str += f"{top_sale.iloc[row,5]:<15}       ${top_sale.iloc[row,6]:<60}\n"
+        result_str += f"{top_sale.iloc[row,5]:<15}       ${top_sale.iloc[row, 6]:<60}\n"
         return result_str
 
 print(question_4(1))
